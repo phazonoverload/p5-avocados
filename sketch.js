5c9b924d163442ce16280034e34350503bf80b27
Kevin Lewis <kevin@jellybabi.es>
2016-09-17 18:56:56 +0100

    Grid of avocados

--- sketch.js
+++ sketch.js
@@ -2,15 +2,27 @@ var movers = [];
 var avocado;
 var colorBg = '#18DA8D';
 var colorGrid = '#9046FF';
+var gridSpacing = 40;
 
 function generateGrid() {
   strokeWeight(3);
   stroke(colorGrid);
-  for (var w = 0; w < width; w += 20) {
+  for (var w = 0; w < width; w += gridSpacing) {
     line(w, 0, w, height);
   }
-  for (var h = 0; h < height; h += 20) {
+  for (var h = 0; h < height; h += gridSpacing) {
     line(0, h, width, h);
+  }
+}
+
+function generateAvocado() {
+  var sizeHeight = gridSpacing * 1.5;
+  var sizeWidth = (sizeHeight / 10) * 7;
+  
+  for(var a = gridSpacing / 4; a < height; a += gridSpacing * 2) {
+    for(var b = gridSpacing / 2; b < width; b += gridSpacing * 2) {
+      image(avocado, b, a, sizeWidth, sizeHeight); 
+    }
   }
 }
 
@@ -25,4 +37,5 @@ function setup() {
 
 function draw() {
   generateGrid();
+  generateAvocado();
 }

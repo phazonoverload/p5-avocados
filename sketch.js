c4170373de2bc5cf4798d5f629d3d34a2f1d7732
Kevin Lewis <kevin@jellybabi.es>
2016-09-17 18:08:16 +0100

    background grid generated

--- sketch.js
+++ sketch.js
@@ -1,7 +1,28 @@
+var movers = [];
+var avocado;
+var colorBg = '#18DA8D';
+var colorGrid = '#9046FF';
+
+function generateGrid() {
+  strokeWeight(3);
+  stroke(colorGrid);
+  for (var w = 0; w < width; w += 20) {
+    line(w, 0, w, height);
+  }
+  for (var h = 0; h < height; h += 20) {
+    line(0, h, width, h);
+  }
+}
+
+function preload() {
+  avocado = loadImage('assets/avocado.svg');
+}
+
 function setup() {
-  
+  createCanvas(windowWidth, windowHeight);
+  background(colorBg);
 }
 
 function draw() {
-  
+  generateGrid();
 }

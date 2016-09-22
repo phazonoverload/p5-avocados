3e2d05fe9eec9ada896701c792258f2467bbece2
Kevin Lewis <kevin@jellybabi.es>
2016-09-22 17:44:13 +0100

    Gemme some of dem avocados

--- sketch.js
+++ sketch.js
@@ -1,11 +1,10 @@
-var movers = [];
-var avocado;
-var colorBg = '#18DA8D';
+var avocados = [];
+var colorBg = '#FC62D0';
 var colorGrid = '#9046FF';
-var gridSpacing = 40;
+var gridSpacing = 30;
 
 function generateGrid() {
-  strokeWeight(3);
+  strokeWeight(1.5);
   stroke(colorGrid);
   for (var w = 0; w < width; w += gridSpacing) {
     line(w, 0, w, height);
@@ -15,27 +14,38 @@ function generateGrid() {
   }
 }
 
-function generateAvocado() {
-  var sizeHeight = gridSpacing * 1.5;
-  var sizeWidth = (sizeHeight / 10) * 7;
-  
-  for(var a = gridSpacing / 4; a < height; a += gridSpacing * 2) {
-    for(var b = gridSpacing / 2; b < width; b += gridSpacing * 2) {
-      image(avocado, b, a, sizeWidth, sizeHeight); 
-    }
+function Avocado() {
+  this.x = random(0, width);
+  this.y = random(0, height);
+  this.scale = random(0.5, 1.5);
+
+  this.create = function() {
+    image(avocadoImg, this.x, this.y, 35 * this.scale, 50 * this.scale);
+  }
+
+  this.move = function() {
+    this.x += random(-1, 1);
+    this.y += random(-1, 1);
   }
 }
 
 function preload() {
-  avocado = loadImage('assets/avocado.svg');
+  avocadoImg = loadImage('assets/avocado.svg');
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(colorBg);
+  for(var i = 0; i < 200; i++) {
+    avocados[i] = new Avocado();
+  }
 }
 
 function draw() {
+  background(colorBg);
   generateGrid();
-  generateAvocado();
+  for(a = 0; a < avocados.length; a++) {
+    avocados[a].create();
+    avocados[a].move();
+  }
 }
